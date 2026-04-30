43301c834ded70467188c5b370470fde9906923a
Nightfallx-owner <nightfallteam02@gmail.com>
2026-04-30 22:31:07 +0500

    products.js

--- products.js
+++ products.js
@@ -1,27 +1,34 @@
 const products=[
-"Burger","Pizza","Chicken","Salad","Rice","Pasta","Juice","Wrap",
-"Soup","Steak","Sushi","Fries","Oats","Smoothie","Avocado Toast",
+"Burger","Pizza","Chicken","Salad","Rice",
+"Pasta","Juice","Wrap","Soup","Steak",
+"Sushi","Fries","Oats","Smoothie","Avocado",
 "Fruit Bowl","Protein Shake","Sandwich","Ice Cream","Noodles"
 ];
 
-let html="";
-products.forEach(p=>{
-html+=`
+let grid=document.getElementById("grid");
+
+let cart=JSON.parse(localStorage.getItem("cart")||"[]");
+
+grid.innerHTML=products.map(p=>`
 <div class="card">
 <img src="https://source.unsplash.com/300x200/?food,${p}">
 <div class="info">
 <h3>${p}</h3>
-<button onclick="order('${p}')">Add Order</button>
+<button onclick="add('${p}')">Add</button>
 </div>
 </div>
-`;
-});
+`).join("");
 
-document.getElementById("grid").innerHTML=html;
+function add(p){
+cart.push(p);
+localStorage.setItem("cart",JSON.stringify(cart));
+alert(p+" added");
+}
 
-function order(p){
-let orders=JSON.parse(localStorage.getItem("orders")||"[]");
-orders.push(p);
-localStorage.setItem("orders",JSON.stringify(orders));
-alert(p+" added to order");
+function goOrder(){
+if(cart.length===0){
+alert("Cart empty!");
+return;
+}
+window.location.href="track.html";
 }
